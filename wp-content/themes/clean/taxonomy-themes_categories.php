14dd1f24cc73caf697d7def0e5a4c024c084c7b4
litsazib <roktimsazib92@gmail.com>
2016-04-06 13:28:56 +0600

    design texonomy page

--- wp-content/themes/clean/taxonomy-themes_categories.php
+++ wp-content/themes/clean/taxonomy-themes_categories.php
@@ -5,20 +5,72 @@
  */
 
 get_header(); ?>
-
-	<h1>Taxonomy archive page </h1>
-
-	<h1><?php the_title();?></h1>
-	<?php the_post_thumbnail('medium');?>
-	
-	
-	<?php if (class_exists('MultiPostThumbnails')) :
-    MultiPostThumbnails::the_post_thumbnail(
-        get_post_type(),
-        'secondary-image'
-    );
-endif; ?>	
-
-
-
+  <section>
+    <div class="banner"> <img class="img-responsive" src="<?php echo get_stylesheet_directory_uri();?>/images/banner.png" alt="banner">
+      <div class="container">
+        <div class="ban_con">
+          <div class="row">
+            <div class="col-sm-4">
+              <h2>Home Spring Cleaning</h2>
+            </div>
+            <div class="col-sm-8"> <img style="padding-top:15px" class="img-responsive" src="<?php echo get_stylesheet_directory_uri();?>/images/spring_girl.png" alt="man"> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="contnt">
+      <div class="container">
+        <div class="main">
+          <div class="col-sm-4">
+            <div class="lft_sdbar">
+              <h2>Services Provided</h2>
+              <ul>
+                <li><a href="javascript:void(0)">House Cleaning</a></li>
+                <li><a href="javascript:void(0)">commercial Cleaning</a></li>
+                <li><a href="javascript:void(0)">window Cleaning</a></li>
+                <li><a href="javascript:void(0)">green Cleaning</a></li>
+                <li><a href="javascript:void(0)">residential Cleaning</a></li>
+                <li><a href="javascript:void(0)">restaurant Cleaning</a></li>
+                <li><a href="javascript:void(0)">ceramic tile Cleaning</a></li>
+                <li><a href="javascript:void(0)">floor care Cleaning</a></li>
+                <li><a href="javascript:void(0)">other special Services</a></li>
+              </ul>
+              <div class="con_info">
+                <h3>MORE INFORMATION</h3>
+                <p>Tim Wilson,<br>
+                  New Street CA 7854, Park Avenue<br>
+                  Sydney 25.<br>
+                  +(01) 800 527 4800<span>timwilson@gmail.com</span></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-8">
+            <div class="windw_contnt">
+              <h2>window Cleaning</h2>
+              <a href="javascript:void(0)"><img  src="<?php echo get_stylesheet_directory_uri();?>/images/springwomen.png" alt="window"></a>
+              <p>Aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quis quaml est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore etas dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit labo mair osam, nisi ut aliquid ex ea commodi consequatur.</p>
+            </div>
+            <div class="row">
+              <div class="col-sm-6">
+                <div class="wnd_cnt"><a href="javascript:void(0)"> <img  src="<?php echo get_stylesheet_directory_uri();?>/images/spring_grl.png" alt="window"></a>
+                  <h3><a href="javascript:void(0)">Quidem rerum facilis est et expedita distinctio</a></h3>
+                  <p>Voluptatem sequi nesciunt. Neque porro quis quaml est, quilas dolorem ipsum quia dolor sit amet, consectetur, adipisci velitar sed quia non numquam eius modi tempora.</p>
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="wnd_cnt"> <a href="javascript:void(0)"><img  src="<?php echo get_stylesheet_directory_uri();?>/images/spring.png" alt="window"></a>
+                  <h3><a href="javascript:void(0)">Dolores et quas molestias excepturi sint occaecati</a></h3>
+                  <p>Voluptatem sequi nesciunt. Neque porro quis quaml est, quilas dolorem ipsum quia dolor sit amet, consectetur, adipisci velitar sed quia non numquam eius modi tempora.</p>
+                </div>
+              </div>
+            </div>
+            <div class="wnd_btn"> <a href="javascript:void(0)">Schedule your window cleaning estimate today!</a> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
 <?php get_footer();?>
